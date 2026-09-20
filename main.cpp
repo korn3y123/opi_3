0b4653d7ff41fedab1b3129a0f282f34412d4816
sigma <nbezborodyh31@gmail.com>
2026-09-20 13:25:08 +0300

    hello

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int age;
+
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    if (age >= 18) {
+        std::cout << "You are an adult";
+    } else {
+        std::cout << "You are a minor";
+    }
+
+    return 0;
+}
