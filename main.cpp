7acf0f015125156587d577a739c1b724c2340d87
Nikitosbarbboss <nbezborodyh31@gmail.com>
2026-09-21 14:56:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 
-int main() {
-    int age;
-
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-
-    if (age >= 18) {
-        std::cout << "You are an adult";
-    } else {
-        std::cout << "You are a minor";
-    }
-
-    return 0;
+void myFunction()
+{
+    int a = 10;
+    int b = 20;
+    int result = a + b;
 }
