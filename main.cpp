d1088a45b08405659193ca5db3b40fa4119e0707
404bonskiyy <404bonskiyy@gmail.com>
2026-09-21 14:46:15 +0400

    Remove main function and clean up code

--- main.cpp
+++ main.cpp
@@ -6,8 +6,3 @@ std::string reverseStr(std::string s) {
     std::reverse(s.begin(), s.end());
     return s;
 }
-
-int main() {
-    std::cout << reverseStr("hello") << std::endl; // olleh
-    return 0;
-}
